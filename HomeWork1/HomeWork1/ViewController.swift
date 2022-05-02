4e299ed2ed0097799f11fb8c45126a90591bb710
AlexMagwitch <alex.senyuta@mail.ru>
2022-05-03 01:33:36 +0300

    Complete HomeWork1

---
+++ HomeWork1/HomeWork1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by Саша Сенюта on 02.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
